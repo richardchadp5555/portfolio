ebd11501f1fb6cec775190ff3c4c068a8d8187f7
Richard Chadwick <richardchadp@gmail.com>
2025-01-25 20:44:51 +0100

    Traduciendo el portfolio a ingles

--- src/app/components/web-projects/web-projects.component.ts
+++ src/app/components/web-projects/web-projects.component.ts
@@ -8,16 +8,16 @@ import { Component } from '@angular/core';
 export class WebProjectsComponent {
   projects = [
     {
-      title: 'Proyecto Cine',
+      title: 'Cinema web project',
       description:
-        'Una página web para un cine, creada usando HTML, CSS y JavaScript.',
+        'website for a fictional cinema, created using HTML, CSS and JavaScript.',
       img: './assets/dam-cinema.JPG',
       link: 'https://richardchadp5555.github.io/cine/',
     },
     {
       title: 'gifs-search app',
       description:
-        'Esta aplicación permite buscar y ver gifs, desarrollado en Angular.',
+        'This app allows you to search and view gifs, developed in Angular',
       img: './assets/gifs-app.JPG',
       link: 'https://richardchadp5555.github.io/gif-search-app/',
     },
